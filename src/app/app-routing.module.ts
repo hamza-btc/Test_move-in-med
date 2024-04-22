f60da3e5d0718b732070925ddf4331c25cda7313
RACHID Hamza <rachidhmz@gmail.com>
2024-04-22 18:08:16 +0200

    integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+// { path: '', component: HomepageComponent },
+{path : 'homepage',component:HomepageComponent},
+{path : 'loginepage',component:LoginpageComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-move-in-med';
+
+  constructor(private router: Router) {}
+
+
+  goToHome() {
+    this.router.navigate(['/homepage']); 
+  }
+
+  goToLogin() {
+    this.router.navigate(['/loginepage']); 
+  }
+
 }
